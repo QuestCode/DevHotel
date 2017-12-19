aaa0e711f30e6b0c0535dea21b30ffeaf83573d6
QuestCode <omgitszslick@gmail.com>
2017-12-19 13:32:20 -0800

    Remove UITests

--- DevHotel/View Controllers/SelectRoomTableViewController.swift
+++ DevHotel/View Controllers/SelectRoomTableViewController.swift
@@ -21,12 +21,7 @@ class SelectRoomTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
+        self.title = "Select Room Type"
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
 
@@ -38,12 +33,10 @@ class SelectRoomTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return RoomType.all.count
     }
 
